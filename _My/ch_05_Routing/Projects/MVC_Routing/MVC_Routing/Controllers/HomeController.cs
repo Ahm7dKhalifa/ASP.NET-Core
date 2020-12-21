1649ac62c79529840bf6cb172c2d6c1f7614c3b7
ahmed khalifa <ahm7dkhalifa@gmail.com>
2020-12-21 19:19:20 +0200

    ADD _My\ch_05_Routing Folder

---
+++ _My/ch_05_Routing/Projects/MVC_Routing/MVC_Routing/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MVC_Routing.Models;
+
+namespace MVC_Routing.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Details(string info)
+        {
+            ViewBag.Details = info;
+            return View();
+        }
+
+        [Route("ApplicationPrivacy")]
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
